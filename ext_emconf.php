94f8b6425304aeaf2c09202858b9a2f29040b54b
Alexander Langer <alex@rheuma-online.de>
2013-01-19 18:20:17 +0100

    Initial import: Import calendar 1.1.4

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "calendar".
+#
+# Auto generated 01-12-2012 21:14
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Extended Calendar',
+	'description' => 'A full-featured calendar.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.1.4',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => 'uploads/tx_calendar/rte/',
+	'modify_tables' => 'tt_content',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Alexander Langer',
+	'author_email' => 'alex@big.endian.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:34:{s:21:"ext_conf_template.txt";s:4:"339c";s:12:"ext_icon.gif";s:4:"aadc";s:17:"ext_localconf.php";s:4:"9bd8";s:14:"ext_tables.php";s:4:"dd9d";s:14:"ext_tables.sql";s:4:"3c86";s:28:"ext_typoscript_editorcfg.txt";s:4:"c2bc";s:24:"ext_typoscript_setup.txt";s:4:"6719";s:15:"flexform_ds.xml";s:4:"4c89";s:24:"icon_tx_calendar_cat.gif";s:4:"c951";s:25:"icon_tx_calendar_item.gif";s:4:"aadc";s:30:"icon_tx_calendar_organizer.gif";s:4:"4327";s:32:"icon_tx_calendar_targetgroup.gif";s:4:"79e7";s:16:"locallang_db.php";s:4:"887b";s:17:"locallang_tca.php";s:4:"95e7";s:7:"tca.php";s:4:"5758";s:11:"tca_dyn.php";s:4:"826f";s:14:"doc/manual.sxw";s:4:"4ba3";s:29:"pi1/class.tx_calendar_pi1.php";s:4:"a7f9";s:17:"pi1/locallang.php";s:4:"0c25";s:31:"res/calendar_inject_tt_news.php";s:4:"fc43";s:25:"templates/Unbenannt-1.jpg";s:4:"3102";s:32:"templates/calendar_template.html";s:4:"99ef";s:27:"templates/day-eventback.jpg";s:4:"10ca";s:18:"templates/day.html";s:4:"aa0e";s:22:"templates/day.html.old";s:4:"5df4";s:24:"templates/dayback-20.jpg";s:4:"86d2";s:20:"templates/event.html";s:4:"6dde";s:21:"templates/fullday.jpg";s:4:"eced";s:18:"templates/left.jpg";s:4:"cf7a";s:25:"templates/month-mini.html";s:4:"82ba";s:20:"templates/month.html";s:4:"e190";s:19:"templates/right.jpg";s:4:"97da";s:23:"templates/upcoming.html";s:4:"7e78";s:19:"templates/week.html";s:4:"649f";}',
+);
+
+?>
